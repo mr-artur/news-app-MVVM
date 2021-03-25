ccfe671815f0d93ee2c2124ae6444dc23dd80f4a
mr-artur <ar2r.pv@gmail.com>
2021-03-25 13:23:50 +0200

    finished db operations with articles, finished saved news functionality
    1. subscribing on saved news in saved news fragment
    2. created touch helper to delete news by swiping
    3. added saved news fragment layout
    4. fixed padding of recycler views (found a bug with divider item decoration, it is also present in the end of recycler view)

--- app/src/main/java/com/arturo/news/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/arturo/news/ui/fragments/SavedNewsFragment.kt
@@ -6,12 +6,16 @@ import android.view.View
 import androidx.core.content.res.ResourcesCompat
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.arturo.news.R
 import com.arturo.news.adapters.NewsAdapter
 
-import kotlinx.android.synthetic.main.fragment_breaking_news.*
+import com.google.android.material.snackbar.Snackbar
+
+import kotlinx.android.synthetic.main.fragment_saved_news.*
 
 class SavedNewsFragment : BaseFragment(R.layout.fragment_saved_news) {
 
@@ -20,6 +24,7 @@ class SavedNewsFragment : BaseFragment(R.layout.fragment_saved_news) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpAdapter()
+        subscribeOnSavedNews();
         setUpRecyclerView()
     }
 
@@ -31,10 +36,16 @@ class SavedNewsFragment : BaseFragment(R.layout.fragment_saved_news) {
         }
     }
 
+    private fun subscribeOnSavedNews() {
+        viewModel.getSavedNews().observe(viewLifecycleOwner, { articles ->
+            newsAdapter.differ.submitList(articles)
+        })
+    }
+
     private fun setUpRecyclerView() {
         val dividerDrawable =
             ResourcesCompat.getDrawable(resources, R.drawable.news_middle_divider, null)
-        rvBreakingNews.apply {
+        rvSavedNews.apply {
             adapter = newsAdapter
             addItemDecoration(
                 DividerItemDecoration(
@@ -45,6 +56,34 @@ class SavedNewsFragment : BaseFragment(R.layout.fragment_saved_news) {
                 }
             )
             layoutManager = LinearLayoutManager(activity)
+        }
+        ItemTouchHelper(itemTouchHelperCallback).apply {
+            attachToRecyclerView(rvSavedNews)
+        }
+    }
+
+    private val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
+        ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+        ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+    ) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val position = viewHolder.adapterPosition
+            val article = newsAdapter.differ.currentList[position]
+            viewModel.deleteArticle(article)
+            Snackbar.make(view!!, "Successfully deleted article", Snackbar.LENGTH_LONG).apply {
+                setAction(getString(R.string.action_undo)) {
+                    viewModel.saveArticle(article)
+                }
+                show()
+            }
         }
     }
 }
